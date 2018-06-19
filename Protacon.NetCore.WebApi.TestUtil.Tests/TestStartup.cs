8a77d18b9688cb8563fb70c4229ea8681f8a975f
Savpek <savolainen.pekka@gmail.com>
2018-06-19 14:07:08 +0300

    Fixed test runs on linux.

--- Protacon.NetCore.WebApi.TestUtil.Tests/TestStartup.cs
+++ Protacon.NetCore.WebApi.TestUtil.Tests/TestStartup.cs
@@ -4,30 +4,28 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Protacon.NetCore.WebApi.TestUtil.Tests.Dummy;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 
 namespace Protacon.NetCore.WebApi.TestUtil.Tests
 {
     public class TestStartup
     {
-        private readonly Startup _original;
-
         public TestStartup(IHostingEnvironment env)
         {
-            _original = new Startup(env);
         }
-
 
         public void ConfigureServices(IServiceCollection services)
         {
-            _original.ConfigureServices(services);
-
-            services.RemoveService<IExternalDepency>()
-                .AddSingleton(Substitute.For<IExternalDepency>());
+            services.AddMvc();
+            services.AddSingleton(Substitute.For<IExternalDepency>());
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            _original.Configure(app, env, loggerFactory);
+            loggerFactory.AddDebug();
+            app.UseMvc();
         }
     }
 }
